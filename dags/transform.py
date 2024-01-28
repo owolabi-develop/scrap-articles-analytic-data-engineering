80f1080be950c94d915ebcf5be5de6302a8e83e6
owolabi-develop <owolabidevelop84@gmail.com>
2024-01-27 16:14:46 -0800

    add snowpip asw sqs auto ingest data from stage into raw table

--- dags/transform.py
+++ dags/transform.py
@@ -5,10 +5,11 @@ def transform_data(extractpath,transformpath):
     csv_files = glob.glob(f'{extractpath}/*.csv')
 
     # Initialize an empty list to store DataFrames
-    data_list = (pd.read_csv(file,sep=',').drop_duplicates().dropna() for file in csv_files)
-    
+    data_list = (pd.read_csv(file,sep=',',parse_dates=True).drop_duplicates().dropna() for file in csv_files)
     
     big_df = pd.concat(data_list,ignore_index=True)
+
+    big_df['retrieval_date'] = pd.to_datetime(big_df['retrieval_date'])
 
     print(big_df.head())
     
@@ -16,3 +17,6 @@ def transform_data(extractpath,transformpath):
         os.makedirs(transformpath)
     # Write the concatenated data to the 'tranformdata.csv' file
     big_df.to_json(f"{transformpath}/tranformdata.json",orient='records')
+
+#transform_data('latestArticles/extract','latestArticles/transform')
+    
--- snowflakemodel/load_data.sql
+++ snowflakemodel/load_data.sql
@@ -1,11 +1,25 @@
 
-
+use role ACCOUNTADMIN;
 list @articles_s3_stage/;
 
-
-copy into raw
+use role ACCOUNTADMIN;
+-- auto ingest data snowpip
+create or replace pipe LATESTARTICLES.ingestpip.article_pips auto_ingest=true as
+copy into LATESTARTICLES.RAW_NEWSARTICLES.raw
     from @articles_s3_stage/
     FILE_FORMAT = (FORMAT_NAME = article_csv_format);
    
 
-select articles:headlines::string as headlines, articles:links::string as links from raw;
+SELECT 
+    articles:headlines::STRING AS headlines,
+    articles:links::STRING AS links,
+    articles:image::STRING AS image,
+    articles:summary::STRING AS summary,
+    TO_TIMESTAMP(articles:retrieval_date) AS retrieval_date,
+    articles:website::STRING AS website,
+    articles:maincountry::STRING AS country
+FROM LATESTARTICLES.RAW_NEWSARTICLES.raw;
+
+
+
+SHOW PIPES;
